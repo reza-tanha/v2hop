6b55822c8f93b686274e68dea65bfcef5146f011
zankoAn <zankoan4@gmail.com>
2023-03-04 04:56:06 +0330

    Double the cost of tunnel servers at the time of purchase

--- hope/bot/views.py
+++ hope/bot/views.py
@@ -362,88 +362,46 @@ def callback_query_update(update):
             reply_markup=show_country_buttons(volume=selected_volume)
         )
 
-    elif callback_data.startswith("plan_country"):
-        uuid = str(uuid4())
-        selected_countr, selected_volume, section = callback_data.split("_")[-1].split(":")
-        server = Server.objects.filter(
-            domain_country=selected_countr, down=False).first()
+    elif callback_data.startswith("buy_plan"):
+        selected_country, selected_volume = callback_data.split("_")[-1].split(":")
+        server = get_object_or_404(Server, domain_country=selected_country, down=False)
         xray = XUIAPI(
             server=server.ip,
             username=server.username,
             password=server.password
         )
-        
         all_configs_in_server = xray.get_count_config()
-        # return
-        # print(len(all_configs_in_server))
         if len(all_configs_in_server) >= 50:
-            server.down=True
+            server.down = True
             server.save()
             telegram.send_AnswerCallbackQuery(
-                callback_id, MESSAGES["message_server_max_config"]
-            )
+                callback_id, MESSAGES["message_server_max_config"])
             admins = User.objects.filter(is_superuser=True)
-            for adm in admins:
+            for admin in admins:
                 telegram.send_Message(
-                    adm.user_id,
-                    message_admin_server_full_count(server),
-                    parse_mode="html"
+                    admin.user_id,
+                    server_full_config_msg(server)
                 )
-            return      
+            return
 
-        if section == "test":
-            # print(section)
-            time_now = datetime.now(tz=pytz.UTC)
-            weektime = time_now + timedelta(7)
-            user.user_balance.test_date = weektime
-            select_config = xray.getSession(
-                expire_date=1, 
-                total=150,
-                uuid=uuid
-            )
-            config = ConfigVpn(
-                conf=select_config[0],
-                server=server,
-                volume=selected_volume,
-                is_use=True,
-                expire_date=datetime.now() + timedelta(hours=1),
-                user=user,
-                uuid=uuid,
-                last_num_change=0
-            )
-            config.save()
-            user.user_balance.save()
-            logging.addlog("hope/logs/test_config.log",
-                           f"user_id: {callback_chat_id}, location: {server.name}:{server.ip}, uuid: {uuid}")
-            telegram.editMessageText(
-                callback_chat_id,
-                callback_message_id,
-                show_config_info(select_config),
-                reply_markup=show_start_home_buttons(callback_chat_id),
-                parse_mode="html"
-            )
-
-            return telegram.send_Message(
-                callback_chat_id,
-                show_config_info(select_config),
-                parse_mode="html"
-            )
-
-            
-        price = Subscribe.objects.filter(volume=selected_volume).first()
-        if user.user_balance.balance < price.price:
+        plan = get_object_or_404(Plan, volume=selected_volume)
+        if user.user_balance.balance < plan.price:
             return telegram.editMessageText(
                 callback_chat_id,
                 callback_message_id,
                 MESSAGES['message_not_balance'],
                 reply_markup=show_start_home_buttons(callback_chat_id))
         else:
-            user.user_balance.balance -= price.price
+            if server.is_tunnel:
+                user.user_balance.balance -= (plan.price * 2)
+            else:
+                user.user_balance.balance -= plan.price
             user.user_balance.save()
 
-        select_config = xray.getSession(24 * 30, price.volume, uuid)
-        config = ConfigVpn(
-            conf=select_config[0],
+        uuid = str(uuid4())
+        select_config = xray.getSession(24 * 30, plan.volume, uuid)
+        config = ProxyConfig(
+            proxy_hash=select_config[0],
             server=server,
             volume=selected_volume,
             is_use=True,
@@ -454,78 +412,67 @@ def callback_query_update(update):
         )
         config.save()
         telegram.editMessageText(
-            callback_chat_id,
-            callback_message_id,
+            callback_chat_id, callback_message_id,
             show_config_info(select_config),
-            reply_markup=show_start_home_buttons(callback_chat_id),
-            parse_mode="html"
+            reply_markup=show_start_home_buttons(callback_chat_id)
         )
         telegram.send_Message(
             CHANNEL_PAYED_CONFIG,
             buy_plan_message_log(
-                callback_chat_id,
-                callback_first_name,
-                callback_username,
-                datetime.now(),
-                price.price,
-                config.server.name,
-                price.volume,
-                select_config),
-            parse_mode="html"
+                callback_chat_id, callback_first_name,
+                callback_username, datetime.now(),
+                plan.price, config.server.name,
+                plan.volume, select_config
+            )
         )
         return logging.addlog("real_config.log", 
                               f"""user_id: {callback_chat_id}, volume: {selected_volume},
                               MB location: {server.name}:{server.ip}, uuid: {uuid}""")
 
-    elif callback_data == 'my_service':
-        configs = ConfigVpn.objects.filter(
-            user=user,
+    elif callback_data.startswith("test_plan"):
+        selected_country, selected_volume = callback_data.split("_")[-1].split(":")
+        server = get_object_or_404(Server, domain_country=selected_country, down=False)
+        uuid = str(uuid4())
+        xray = XUIAPI(
+            server=server.ip,
+            username=server.username,
+            password=server.password
+        )
+        select_config = xray.getSession(
+            expire_date=1,
+            total=150,
+            uuid=uuid
+        )
+        config = ProxyConfig(
+            proxy_hash=select_config[0],
+            server=server,
+            volume=selected_volume,
             is_use=True,
-            volume__gt=150
+            expire_date=datetime.now() + timedelta(hours=1),
+            user=user,
+            uuid=uuid,
+            last_num_change=0
         )
-        if configs.count() == 0:
-            return telegram.editMessageText(
-                callback_chat_id,
-                callback_message_id,
-                MESSAGES['message_not_service'],
-                reply_markup=show_start_home_buttons(callback_chat_id),
-            )
-
-        if configs.count() < 5:
-            return telegram.editMessageText(
-                callback_chat_id,
-                callback_message_id,
-                "سرویس های شما",
-                reply_markup=ServicesButton(configs, callback_chat_id)
-            )
-
-        conf_ = []
-        counter = 1
-        for conf in configs:
-            if counter <= 5:
-                conf_.append(conf)
-                continue
-            else:
-                telegram.send_Message(
-                    callback_chat_id,
-                    "سرویس های شما",
-                    reply_markup=ServicesButton(conf_, callback_chat_id)
-                )
-                counter = 1
-                conf_ = []
-
-        telegram.send_Message(
+        config.save()
+        weektime = datetime.now(tz=pytz.UTC) + timedelta(7)
+        user.user_balance.test_date = weektime
+        user.user_balance.save()
+        logging.addlog("test_config.log",
+                       f"user_id: {callback_chat_id}, location: {server.name}:{server.ip}, uuid: {uuid}")
+        telegram.editMessageText(
             callback_chat_id,
-            "سرویس های شما",
-            reply_markup=ServicesButton(conf_, callback_chat_id)
+            callback_message_id,
+            show_config_info(select_config, True)
         )
-        counter = 1
-        conf_ = []
-        return
+        return telegram.send_Message(
+            callback_chat_id,
+            MESSAGES["start_message"],
+            reply_markup=show_start_home_buttons(callback_chat_id)
+        )
 
     elif callback_data.startswith("service"):
         _, config_id = callback_data.split("_")[-1].split(":")
-        config = ConfigVpn.objects.get(id=config_id)
+        config = get_object_or_404(ProxyConfig, id=config_id)
         server = config.server
         xray = XUIAPI(server.ip, server.username, server.password)
         config_info = xray.get_config_uuid(config.uuid)        
@@ -547,110 +494,42 @@ def callback_query_update(update):
             callback_chat_id,
             callback_message_id,
             text,
-            reply_markup=change_config_location_buttons(config.id),
-            parse_mode="html"
-        )
-
-    elif callback_data == 'test_config':
-        time_now = datetime.now(tz=pytz.UTC)
-        weektime = time_now + timedelta(7)
-        
-        if user.user_balance.test_date:
-            if user.user_balance.test_date > time_now:
-                return telegram.editMessageText(
-                    callback_chat_id,
-                    callback_message_id,
-                    MESSAGES['message_error_test_config'],
-                    reply_markup=show_start_home_buttons(callback_chat_id)
-                )
-        return telegram.editMessageText(
-            callback_chat_id,
-            callback_message_id,
-            MESSAGES['message_get_country'],
-            reply_markup=show_country_buttons(section="test")
+            reply_markup=change_config_location_buttons(config.id)
         )
 
     elif callback_data.startswith("change_location:"):
         data = callback_data.split(":")[1]
-        self_config = ConfigVpn.objects.get(id=data)
+        self_config = get_object_or_404(ProxyConfig, id=data)
         if self_config.last_num_change > 3:
             return telegram.send_AnswerCallbackQuery(
                 callback_id,
-                "⛔️ شما فقط 3 بار قادر به عوض کردن کانفیگ خود هستید ⛔️"
+                MESSAGES["message_change_location_limit_error"]
             )
-            
-            
+
         return telegram.editMessageText(
             callback_chat_id,
             callback_message_id,
-            MESSAGES['message_get_country'],
+            MESSAGES['message_choice_country'],
             reply_markup=show_change_location_country(data)
         )
 
     elif callback_data.startswith("change_location_country"):
         config_id, country = callback_data.split("_")[-1].split(":")
-        uuid = str(uuid4())
-        new_server = Server.objects.filter(
-            down=False, domain_country=country).first()
-        config = ConfigVpn.objects.filter(id=config_id).first()
-        last_server = config.server
-        xray = XUIAPI(
-            server=last_server.ip,
-            username=last_server.username,
-            password=last_server.password
-        )
-        current_config = xray.get_config_uuid(config.uuid)
-        new_volume = current_config['total'] - (current_config['down'] + current_config['up'])
-        new_volume = int(new_volume / 1024 / 1024)
-
-        if config.expire_date <= datetime.now().date():
-            return telegram.send_AnswerCallbackQuery(
-                callback_id, MESSAGES["message_config_expire_error"]
-            )
-
-        if new_volume <= 500:  # if volume < 500 mb
-            return telegram.send_AnswerCallbackQuery(
-                callback_id,
-                "حجم شما کمتر از 500 مگابایت است و قابلت تغیر لوکیشن ندارید"
-            )
-
-        try:
-            xray.delete_config(current_config['id'])
-            xray = XUIAPI(
-                server=new_server.ip,
-                username=new_server.username,
-                password=new_server.password
-            )
-            select_config = xray.getSession(
-                current_config['expiryTime'], new_volume, uuid, True)
-        except:
-            return
-        num_change_location = config.last_num_change+1
-        new_config = ConfigVpn(
-            conf=select_config[0],
-            server=new_server,
-            volume=new_volume,
-            is_use=True,
-            expire_date=config.expire_date,
-            user=user,
-            uuid=uuid,
-            last_num_change=num_change_location
-        )
-        new_config.save()
-        config.delete()
-        return telegram.editMessageText(
-            callback_chat_id,
-            callback_message_id,
-            show_config_info(select_config),
-            reply_markup=show_start_home_buttons(callback_chat_id),
-            parse_mode="html"
-        )
+        new_server = get_object_or_404(Server, domain_country=country, down=False)
+        ChangeLocation(
+            telegram, user,
+            new_server,
+            callback_chat_id, callback_message_id,
+            str(uuid4()), 
+            config_id,
+            callback_id
+        ).main()
 
     elif callback_data.startswith("delete_service"):
         ip, config_uuid = callback_data.split("_")[-1].split(":")
         service_id = callback_text.split("ID: ")[1].split("\n")[0]
         server = Server.objects.filter(ip=ip).first()
-        config = ConfigVpn.objects.filter(uuid=config_uuid)
+        config = ProxyConfig.objects.filter(uuid=config_uuid)
         xui = XUIAPI(
             server=ip,
             username=server.username,
@@ -669,7 +548,8 @@ def callback_query_update(update):
                 callback_chat_id,
                 callback_message_id,
                 callback_text,
-                entities=json.dumps([ {'offset': 0, 'length': len(callback_text), 'type': 'strikethrough'}])
+                entities=json.dumps(
+                    [{'offset': 0, 'length': len(callback_text), 'type': 'strikethrough'}])
             )
             return telegram.send_Message(
                 chat_id=callback_chat_id,
