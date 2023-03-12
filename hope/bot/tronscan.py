960c23fb63005cea9f3b7b5e7898c4e21f741a3d
zankoAn <zankoan4@gmail.com>
2023-03-12 23:32:41 +0330

    CleanUp

--- hope/bot/tronscan.py
+++ hope/bot/tronscan.py
@@ -1,72 +1,52 @@
 import requests
 
 
-class TTC:
+class TronScan:
     def __init__(self) -> None:
-        self.base_api = "https://apilist.tronscan.org/api/transaction-info?hash="
-        
-    def check(self, hash) -> dict:
-        try : 
-            req = requests.get(self.base_api + hash)
-            js = req.json()
-            toAddress = js.get('toAddress', None)
-            ownerAddress = js.get('ownerAddress', None)
-            tokenTransferInfo=js.get('tokenTransferInfo', None)
-            contractRet=js.get('contractRet', None)#SUCCESS
-            amount = js['contractData'].get('amount', None)
-            srConfirmList = len(js['srConfirmList']) #if < 6 == False
+        self.base_api = "https://apilist.tronscan.org/api/transaction-info?hash={}"
+
+    def check(self, transaction_id: str) -> dict | bool:
+        """Get information about transaction id"""
+        try:
+            response = requests.get(self.base_api.format(transaction_id))
+            data = response.json()
+            transfer_info = data.get('tokenTransferInfo')
+            amount = data.get('contractData', {}).get('amount')
             contract_address = None
-            if tokenTransferInfo:
-                contract_address = tokenTransferInfo.get('contract_address', None)#if tron == None
-                amount = js["tokenTransferInfo"].get("amount_str", None)
-                
+            if transfer_info:
+                contract_address = transfer_info.get('contract_address')
+                amount = transfer_info.get("amount_str")
+
             return {
-                "contractRet": contractRet,
-                "ownerAddress": ownerAddress,
-                "toAddress": toAddress,
-                "amount": int(amount) / 1000000,
-                "srConfirmList": srConfirmList,
-                "contract_address": contract_address
+                "contractRet": data.get('contractRet'),
+                "ownerAddress": data.get('ownerAddress'),
+                "toAddress": data.get('toAddress'),
+                "contract_address": contract_address,
+                "srConfirmList": len(data.get('srConfirmList')),
+                "amount": int(amount) / 1000000
             }
         except:
-            return False    
-
-# ttc = TTC()
-# print(ttc.check("9911e9895aa22f0909b68f34040e92181325adb175775ceaf1e3b1b03cd33e98"))
-# print(ttc.check("1ff57b269d245ebae352a554be00f9e952e517f2ba64e19aaa6f381067dff5aa"))
+            return False
 
 
-# class ChangeToRial1:
-#     def __init__(self) -> None:
-#         self.base_api = "https://api.nobitex.ir/v2/orderbook/"
-        
-#     def change(self, symbol) -> dict:
-#         try : 
-#             req = requests.get(self.base_api + symbol)
-#             js = req.json()
-#             return js['lastTradePrice']
-#         except:
-#             return False
-    
-    
-class ChangeToRial:
+class Exchange:
+    """
+    Get price of currency by symbol
+    """
+
     def __init__(self) -> None:
         self.base_api = "https://api.bitpin.ir/v1/mkt/markets/"
-        
-    def change(self, symbol) -> dict:
-        symbol = symbol+"_IRT"
-        try : 
+
+    def get_symbol_price(self, symbol: str) -> dict:
+        symbol += "_IRT"
+        try:
             req = requests.get(self.base_api)
             js = req.json()
             for code in js['results']:
                 if code.get("code") == symbol:
-                    return code.get("price", None)
-            # return js['lastTradePrice']
+                    if code.get("price"):
+                        return int(code.get("price"))
+                    else:
+                        return None
         except:
             return False
-    
-    
-# c = ChangeToRial()
-# print(c.change("DOGE"))
-#USDT #USDC #BUSD #BTT #TRX #BTC #ETH #DOGE
-# c.change("USDJ")
