2938545c34aff199941bce9bebd0f897e4356695
zankoAn <zankoan4@gmail.com>
2023-03-13 13:22:32 +0330

    CleanUp

--- hope/bot/validator/payment_validator.py
+++ hope/bot/validator/payment_validator.py
@@ -17,7 +17,7 @@ class PaymentValidator:
             return False, self.MESSAGES['message_transactionid_len_error']
         return True, None
 
-    def check_payment_exists_before(self) -> bool|str|object:
+    def check_payment_exists_before(self) -> bool | str | object:
         """Check to see user transactionID exists before or not
         """
         try:
@@ -30,7 +30,7 @@ class PaymentValidator:
         except:
             return False, self.MESSAGES['message_transaction_id_invalid_error']
 
-    def validate_owner_address(self) -> bool|str:
+    def validate_owner_address(self) -> bool | str:
         """Check the OwnerAddress of the transaction
            is the same as the user wallet address or not
         """
@@ -39,21 +39,21 @@ class PaymentValidator:
             return False, self.MESSAGES["message_user_transaction_not_related_error"]
         return True, None
 
-    def validate_wallet(self) -> bool|str:
+    def validate_wallet(self) -> bool | str:
         """Check whether the user dest transaction address
-            is the same as the admin address"""         
+            is the same as the admin address"""
         wallet = self.wallet.objects.filter(wallet=self.info['toAddress'])
         if not wallet:
             return False, self.MESSAGES["message_user_transaction_dst_invalid_error"]
         return True, None
 
-    def validate_contract_ret(self) -> bool|str:
+    def validate_contract_ret(self) -> bool | str:
         """Check contractRet value and confirm list"""
         if self.info['contractRet'].lower() != "success" and self.info['srConfirmList'] < 6:
             return False, self.MESSAGES['message_transaction_not_verified_error']
         return True, None
 
-    def validate_contract_address(self) -> bool|str|object:
+    def validate_contract_address(self) -> bool | str | object:
         """Check User transaction ContractAdress 
             exists in the valid list or not
         """
@@ -65,32 +65,38 @@ class PaymentValidator:
         return True, contract
 
     def update_user_payment(self, user_payment: object) -> None:
-        user_payment.status=True
-        user_payment.ownerAddress=self.info['ownerAddress']
-        user_payment.amount=self.info['amount']
-        user_payment.contract_address=self.info['contract_address']
-        user_payment.contractRet=self.info['contractRet']
-        user_payment.srConfirmList=self.info['srConfirmList']
+        user_payment.status = True
+        user_payment.ownerAddress = self.info['ownerAddress']
+        user_payment.amount = self.info['amount']
+        user_payment.contract_address = self.info['contract_address']
+        user_payment.contractRet = self.info['contractRet']
+        user_payment.srConfirmList = self.info['srConfirmList']
         user_payment.save()
 
     def main(self):
         status, data = self.check_transactionid_len()
-        if not status: return None, data
+        if not status:
+            return None, data
 
         status, user_payment = self.check_payment_exists_before()
-        if not status: return None, user_payment
+        if not status:
+            return None, user_payment
 
         status, data = self.validate_owner_address()
-        if not status: return None, data
+        if not status:
+            return None, data
 
         status, data = self.validate_wallet()
-        if not status: return None, data
+        if not status:
+            return None, data
 
         status, data = self.validate_contract_ret()
-        if not status: return None, data
+        if not status:
+            return None, data
 
         status, contract = self.validate_contract_address()
-        if not status: return None, contract
+        if not status:
+            return None, contract
 
         self.update_user_payment(user_payment)
         return contract, self.info["amount"]
